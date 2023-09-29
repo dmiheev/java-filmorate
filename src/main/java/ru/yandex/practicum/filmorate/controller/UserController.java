a206976e92597d7126ef5eda5a693c7e211a9717
Dmitry Miheev <dmiheev@mail.ru>
2023-09-29 15:49:57 +0300

    Sprint 9 - filmorate - v1.2.1
    Убраны лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
@@ -15,7 +14,7 @@ public class Film extends AbstractEntity {
     @NotBlank(message = "Название фильма не может быть пустым.")
     private String name;
 
-    @Size(min = 1,max = 200, message = "Максимальная длина описания для фильма — 200 символов.")
+    @Size(min = 1, max = 200, message = "Максимальная длина описания для фильма — 200 символов.")
     private String description;
 
     private LocalDate releaseDate;
